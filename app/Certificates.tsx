1748444c49977e51ca28ae161dfe109dcd064617
GGdid <118999300+GGdid@users.noreply.github.com>
2026-03-04 19:38:02 +0530

    updated content

--- app/Certificates.tsx
+++ app/Certificates.tsx
@@ -14,19 +14,19 @@ export default function Certificates(){
     ];
     
       return (
-        <div className="parent-div flex items-center justify-center w-full overflow-hidden frostblue-bg">
+        <div className="parent-div flex sm:flex-col lg:flex-row min-h-screen items-center justify-center w-full overflow-hidden frostblue-bg">
       
       {/* LEFT: stacked certificates */}
-      <div className="leftContainer h-screen flex justify-center rounded-2xl moonlight-bg px-12 py-20 ml-12 mb-12 border-8 border-[#465c78]">
+      <div className="leftContainer h-screen w-full flex justify-center items-center rounded-2xl moonlight-bg lg:w-1/2 p-6 sm:p-10 lg:p-16 border-8 border-[#465c78]">
         
-      <div className="relative w-[520px] h-[420px]">
+      <div className="relative w-full max-w-[520px] h-[300px] sm:h-[350px] md:h-[400px]">
         <div
           className="absolute top-0 z-20 hover:-translate-y-2 transition border-4 border-[#465c78] rounded-xl cursor-pointer shadow-2xl"
           onClick={() => setActiveIndex(0)}
         >
           <iframe
             src="/certificates/pdf-01.pdf#toolbar=0"
-            className="w-[500px] h-[350px] rounded-xl shadow-xl cert-one pointer-events-none"
+            className="w-[500px] h-[350px] sm:h-[250px] md:h-[300px] rounded-xl shadow-xl cert-one pointer-events-none"
             title="certificate-1"
           />
         </div>
@@ -34,7 +34,7 @@ export default function Certificates(){
         <div className="absolute top-12 z-30 hover:-translate-y-2 transition border-4 border-[#465c78] rounded-xl  shadow-2xl" onClick={()=>setActiveIndex(1)}>
           <iframe
             src="/certificates/pdf-02-csr.pdf#toolbar=0"
-            className="w-[500px] h-[350px] rounded-xl shadow-xl cert-two pointer-events-none"
+            className="w-[500px] h-[350px] sm:h-[250px] md:h-[300px] rounded-xl shadow-xl cert-two pointer-events-none"
             title="certificate-2"
           />
         </div>
@@ -42,7 +42,7 @@ export default function Certificates(){
         <div className="absolute top-24 z-40 hover:-translate-y-2 transition border-4 border-[#465c78] rounded-xl shadow-2xl" onClick={()=>setActiveIndex(2)}>
           <iframe
             src="/certificates/pdf-03-12A-certificate.pdf#toolbar=0"
-            className="w-[500px] h-[350px] rounded-xl shadow-xl bg-white pointer-events-none"
+            className="w-[500px] h-[350px] sm:h-[250px] md:h-[300px] rounded-xl shadow-xl bg-white pointer-events-none"
             title="certificate-3"
           />
         </div>
@@ -50,7 +50,7 @@ export default function Certificates(){
         <div className="absolute top-36 z-40 hover:-translate-y-2 transition border-4 border-[#465c78] rounded-xl shadow-2xl" onClick={()=>setActiveIndex(3)}>
           <iframe 
           src="/certificates/pdf-04-e-anudaan.pdf#toolbar=0" 
-          className="w-[500px] h-[350px] rounded-xl shadow-xl pointer-events-none"
+          className="w-[500px] h-[350px] sm:h-[250px] md:h-[300px] rounded-xl shadow-xl pointer-events-none"
           title="certificate-3"
           />
         </div>
@@ -60,8 +60,8 @@ export default function Certificates(){
       
 
       {/* RIGHT: selected certificate */}
-      <div className="rightCont flex items-center justify-center h-screen frostblue-bg px-12">
-        <div className="relative w-[800px] h-[600px] rounded-xl px-4 border-4 border-neutral-200 overflow-hidden shadow-2xl shadow-neutral-800 bg-[#465c78]">
+      <div className="rightCont w-full lg:w-1/2 flex items-center justify-center min-h-screen frostblue-bg p-6 sm:p-10 lg:p-16">
+        <div className="relative w-full max-w-[900px] h-[350px] sm:h-[450px] md:h-[550px] lg:h-[600px] rounded-xl px-4 border-4 border-neutral-200 overflow-hidden shadow-2xl shadow-neutral-800 bg-[#465c78]">
             {activeIndex !== null ? (
               <iframe
                 src={`${pdfFiles[activeIndex]}#toolbar=0&view=FitH`}
--- app/Donation.tsx
+++ app/Donation.tsx
@@ -2,9 +2,9 @@ import {quicksand , quintessential} from "./ui/fonts";
 export default function Donation(){
     return(
         <>
-        <div className="flex flex-col gap-12 h-screen justify-center items-center">
+        <div className="flex flex-col gap-12 min-h-screen justify-center items-center">
             <div className="w-full">
-                <p className={`${quicksand.className} text-6xl font-semibold flex justify-center mx-20 rounded-[15px] text-[#b9cadf] py-4 mt-[20px]`}>Help us by donating to our cause</p>
+                <p className={`${quicksand.className} sm:text-3xl md:text-5xl font-semibold flex justify-center mx-20 rounded-[15px] text-[#b9cadf] py-4 mt-[20px]`}>Help us by donating to our cause</p>
                 <p className={`${quicksand.className} text-lg flex items-center justify-center`}>click on the button below to donate</p>
             </div>
             <div className="w-full flex justify-center items-center">
